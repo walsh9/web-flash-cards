283a29de8198dd1a1de8eaf4a94048b659458625
Steven Leiva <leiva.steven@gmail.com>
2015-10-03 12:55:46 -0400

    Add a new deck to to the fixtures directory. Changed seeds.rb file to load all decks as long as they follow the naming convention 'deckname-flashcards.txt'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
-deck = Deck.create(name: 'Ruby')
-front = nil
-back = nil
+fixtures_path = APP_ROOT.join('db', 'fixtures')
 
-File.new("db/fixtures/ruby-flashcards.txt").readlines.each_with_index do |line, i|
-  if i % 3 == 0
-    front = line.chomp
-  elsif i % 3 == 1
-    back = line.chomp
-    Card.create(deck_id: deck.id, front: front, back: back)
-  end 
+fixtures_path.each_child do |entry|
+  deck_name = entry.basename('-*').to_s.capitalize
+
+  deck = Deck.create(name: deck_name)
+
+  front = nil
+  back = nil
+
+  File.new(entry).readlines.each_with_index do |line, i|
+    if i % 3 == 0
+      front = line.chomp
+    elsif i % 3 == 1
+      back = line.chomp
+      Card.create(deck_id: deck.id, front: front, back: back)
+    end
+  end
 end
+
